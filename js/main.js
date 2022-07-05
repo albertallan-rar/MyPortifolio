e85b048224ee0ba34616318c1d09323a4345abb1
albertallan <79917446+albertallan-rar@users.noreply.github.com>
2022-07-05 02:06:57 -0300

    Criação do menu e ajustes :tada:
    Foi realizado a criação do menu e reestruturação de alguns elementos do inicio do projeto, inicio da estruturação da sessão "sobre"

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+window.addEventListener('scroll', rolagem)
+
+function rolagem() {
+  showNavOnScroll()
+}
+
 function menuexpansion() {
-  console.log('Menu OK')
+  document.body.classList.add('menu-expanded')
+}
+function exitmenuexpansion() {
+  document.body.classList.remove('menu-expanded')
+}
+function showNavOnScroll() {
+  if (scrollY == 0) {
+    navegador.classList.remove('scroll')
+  } else {
+    navegador.classList.add('scroll')
+  }
 }
